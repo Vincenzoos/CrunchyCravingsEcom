21fd5e12c463a1d66bc7a3731d54e642a6e1b6fc
Vincenzoos <quocvietc18@gmail.com>
2025-03-22 12:50:40 +1100

    feat: Bake contacts table - Initialise the contacts management feature for the website

---
+++ src/Model/Table/ContactsTable.php
@@ -0,0 +1,91 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Table;
+
+use Cake\ORM\Query\SelectQuery;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Contacts Model
+ *
+ * @method \App\Model\Entity\Contact newEmptyEntity()
+ * @method \App\Model\Entity\Contact newEntity(array $data, array $options = [])
+ * @method array<\App\Model\Entity\Contact> newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Contact get(mixed $primaryKey, array|string $finder = 'all', \Psr\SimpleCache\CacheInterface|string|null $cache = null, \Closure|string|null $cacheKey = null, mixed ...$args)
+ * @method \App\Model\Entity\Contact findOrCreate($search, ?callable $callback = null, array $options = [])
+ * @method \App\Model\Entity\Contact patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method array<\App\Model\Entity\Contact> patchEntities(iterable $entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Contact|false save(\Cake\Datasource\EntityInterface $entity, array $options = [])
+ * @method \App\Model\Entity\Contact saveOrFail(\Cake\Datasource\EntityInterface $entity, array $options = [])
+ * @method iterable<\App\Model\Entity\Contact>|\Cake\Datasource\ResultSetInterface<\App\Model\Entity\Contact>|false saveMany(iterable $entities, array $options = [])
+ * @method iterable<\App\Model\Entity\Contact>|\Cake\Datasource\ResultSetInterface<\App\Model\Entity\Contact> saveManyOrFail(iterable $entities, array $options = [])
+ * @method iterable<\App\Model\Entity\Contact>|\Cake\Datasource\ResultSetInterface<\App\Model\Entity\Contact>|false deleteMany(iterable $entities, array $options = [])
+ * @method iterable<\App\Model\Entity\Contact>|\Cake\Datasource\ResultSetInterface<\App\Model\Entity\Contact> deleteManyOrFail(iterable $entities, array $options = [])
+ */
+class ContactsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array<string, mixed> $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config): void
+    {
+        parent::initialize($config);
+
+        $this->setTable('contacts');
+        $this->setDisplayField('first_name');
+        $this->setPrimaryKey('id');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->scalar('first_name')
+            ->maxLength('first_name', 255)
+            ->requirePresence('first_name', 'create')
+            ->notEmptyString('first_name');
+
+        $validator
+            ->scalar('last_name')
+            ->maxLength('last_name', 255)
+            ->requirePresence('last_name', 'create')
+            ->notEmptyString('last_name');
+
+        $validator
+            ->email('email')
+            ->requirePresence('email', 'create')
+            ->notEmptyString('email');
+
+        $validator
+            ->scalar('phone_number')
+            ->maxLength('phone_number', 10)
+            ->requirePresence('phone_number', 'create')
+            ->notEmptyString('phone_number');
+
+        $validator
+            ->scalar('message')
+            ->requirePresence('message', 'create')
+            ->notEmptyString('message');
+
+        $validator
+            ->boolean('replied')
+            ->notEmptyString('replied');
+
+        $validator
+            ->date('date_sent')
+            ->notEmptyDate('date_sent');
+
+        return $validator;
+    }
+}
---
+++ templates/Contacts/view.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Contact $contact
+ */
+?>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Html->link(__('Edit Contact'), ['action' => 'edit', $contact->id], ['class' => 'side-nav-item']) ?>
+            <?= $this->Form->postLink(__('Delete Contact'), ['action' => 'delete', $contact->id], ['confirm' => __('Are you sure you want to delete # {0}?', $contact->id), 'class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('List Contacts'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('New Contact'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+        </div>
+    </aside>
+    <div class="column column-80">
+        <div class="contacts view content">
+            <h3><?= h($contact->first_name) ?></h3>
+            <table>
+                <tr>
+                    <th><?= __('First Name') ?></th>
+                    <td><?= h($contact->first_name) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Last Name') ?></th>
+                    <td><?= h($contact->last_name) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Email') ?></th>
+                    <td><?= h($contact->email) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Phone Number') ?></th>
+                    <td><?= h($contact->phone_number) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Id') ?></th>
+                    <td><?= $this->Number->format($contact->id) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Date Sent') ?></th>
+                    <td><?= h($contact->date_sent) ?></td>
+                </tr>
+                <tr>
+                    <th><?= __('Replied') ?></th>
+                    <td><?= $contact->replied ? __('Yes') : __('No'); ?></td>
+                </tr>
+            </table>
+            <div class="text">
+                <strong><?= __('Message') ?></strong>
+                <blockquote>
+                    <?= $this->Text->autoParagraph(h($contact->message)); ?>
+                </blockquote>
+            </div>
+        </div>
+    </div>
+</div>
