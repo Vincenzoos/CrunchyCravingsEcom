ce6f9b2dd8ebfa543ef0329a3c5a163a606b4110
Viet Tran <vtra0041@student.monash.edu>
2025-03-23 14:54:51 +1100

    - refactor (Contact's pages): Modify index, add and view pages for Contact: - Remove unnecessary fields such as: auto-incremented ID for view and index pages - Use reply_status virtual field for Replied column - Utilize full_name virtual field to in display confirmation message upon contact's record deletion

--- templates/Contacts/view.php
+++ templates/Contacts/view.php
@@ -9,7 +9,7 @@
         <div class="side-nav">
             <h4 class="heading"><?= __('Actions') ?></h4>
             <?= $this->Html->link(__('Edit Contact'), ['action' => 'edit', $contact->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Contact'), ['action' => 'delete', $contact->id], ['confirm' => __('Are you sure you want to delete # {0}?', $contact->id), 'class' => 'side-nav-item']) ?>
+            <?= $this->Form->postLink(__('Delete Contact'), ['action' => 'delete', $contact->id], ['confirm' => __('Are you sure you want to delete this contact: {0} ({1})?', $contact->full_name, $contact->email), 'class' => 'side-nav-item']) ?>
             <?= $this->Html->link(__('List Contacts'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
             <?= $this->Html->link(__('New Contact'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
         </div>
@@ -33,10 +33,6 @@
                 <tr>
                     <th><?= __('Phone Number') ?></th>
                     <td><?= h($contact->phone_number) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($contact->id) ?></td>
                 </tr>
                 <tr>
                     <th><?= __('Date Sent') ?></th>
