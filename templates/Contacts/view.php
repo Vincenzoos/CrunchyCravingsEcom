a5097bf9a074a9c44133569c1f942d568836cb7e
Viet Tran <vtra0041@student.monash.edu>
2025-03-27 11:54:32 +1100

    fix (Contacts view pages): Fix view page, was previously styled as add page - Remove add page content - Add view page content

--- templates/Contacts/view.php
+++ templates/Contacts/view.php
@@ -4,6 +4,7 @@
  * @var \App\Model\Entity\Contact $contact
  */
 ?>
+
 <div id="wrapper">
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
@@ -14,25 +15,51 @@
                     <aside class="column col-lg-3">
                         <div class="side-nav">
                             <h4 class="heading"><?= __('Actions') ?></h4>
+                            <?= $this->Html->link(__('Edit Contact'), ['action' => 'edit', $contact->id], ['class' => 'btn btn-warning btn-block mb-3']) ?>
+                            <?= $this->Form->postLink(__('Delete Contact'), ['action' => 'delete', $contact->id], [
+                                'confirm' => __('Are you sure you want to delete this contact: {0} ({1})?', $contact->full_name, $contact->email),
+                                'class' => 'btn btn-danger btn-block mb-3'
+                            ]) ?>
                             <?= $this->Html->link(__('List Contacts'), ['action' => 'index'], ['class' => 'btn btn-primary btn-block mb-3']) ?>
+                            <?= $this->Html->link(__('New Contact'), ['action' => 'add'], ['class' => 'btn btn-success btn-block mb-3']) ?>
+                            <?= $this->Html->link(__('Update Reply Status'), ['action' => 'updateReplyStatus', $contact->id], ['class' => 'btn btn-info btn-block']) ?>
                         </div>
                     </aside>
                     <div class="column col-lg-9">
-                        <div class="contacts form content">
-                            <h3><?= __('Add Contact') ?></h3>
-                            <?= $this->Form->create($contact) ?>
-                            <fieldset>
-                                <?php
-                                echo $this->Form->control('first_name', ['class' => 'form-control mb-3']);
-                                echo $this->Form->control('last_name', ['class' => 'form-control mb-3']);
-                                echo $this->Form->control('email', ['class' => 'form-control mb-3']);
-                                echo $this->Form->control('phone_number', ['class' => 'form-control mb-3']);
-                                echo $this->Form->control('message', ['class' => 'form-control mb-3']);
-                                echo $this->Form->control('date_sent', ['class' => 'form-control mb-3']);
-                                ?>
-                            </fieldset>
-                            <?= $this->Form->button(__('Submit'), ['class' => 'btn btn-success btn-block']) ?>
-                            <?= $this->Form->end() ?>
+                        <div class="contacts view content">
+                            <h3><?= h($contact->first_name) . ' ' . h($contact->last_name) ?></h3>
+                            <table class="table table-bordered">
+                                <tr>
+                                    <th><?= __('First Name') ?></th>
+                                    <td><?= h($contact->first_name) ?></td>
+                                </tr>
+                                <tr>
+                                    <th><?= __('Last Name') ?></th>
+                                    <td><?= h($contact->last_name) ?></td>
+                                </tr>
+                                <tr>
+                                    <th><?= __('Email') ?></th>
+                                    <td><?= h($contact->email) ?></td>
+                                </tr>
+                                <tr>
+                                    <th><?= __('Phone Number') ?></th>
+                                    <td><?= h($contact->phone_number) ?></td>
+                                </tr>
+                                <tr>
+                                    <th><?= __('Date Sent') ?></th>
+                                    <td><?= h($contact->date_sent) ?></td>
+                                </tr>
+                                <tr>
+                                    <th><?= __('Replied') ?></th>
+                                    <td><?= $contact->replied ? __('Yes') : __('No'); ?></td>
+                                </tr>
+                            </table>
+                            <div class="text">
+                                <strong><?= __('Message') ?></strong>
+                                <blockquote>
+                                    <?= $this->Text->autoParagraph(h($contact->message)); ?>
+                                </blockquote>
+                            </div>
                         </div>
                     </div>
                 </div>
