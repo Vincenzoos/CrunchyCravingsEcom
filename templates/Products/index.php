031a86685bb2fc51d623533910620aff1e0e0c54
Viet Tran <vtra0041@student.monash.edu>
2025-04-20 17:12:45 +1000

    feat(admin products index): Remove redundant css and js link

--- templates/Products/index.php
+++ templates/Products/index.php
@@ -18,17 +18,6 @@ $html = new HtmlHelper(new \Cake\View\View());
     <!-- Custom CSS -->
     <?= $this->Html->css(['utilities', 'table', 'form']) ?>
 
-    <!-- jQuery UI CSS -->
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-
-    <!-- Select2 CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-    <!-- Select2 JS -->
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 </head>
 
 <body>
