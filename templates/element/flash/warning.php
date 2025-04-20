4bde2f708eff86a2f8eedff0814936afcd9dd1d3
Viet Tran <vtra0041@student.monash.edu>
2025-04-20 16:45:22 +1000

    feat (flash message): Add custom styling to flash message - Custom styling in flash.message.css - Bootstrap icon used

--- templates/element/flash/warning.php
+++ templates/element/flash/warning.php
@@ -8,4 +8,4 @@ if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="message warning" onclick="this.classList.add('hidden');"><?= $message ?></div>
+<div class="message warning alert alert-warning" onclick="this.classList.add('hidden');"><i class="fa fa-warning"></i><?= ' '.$message ?></div>
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -40,6 +40,9 @@ $html = new HtmlHelper(new View());
     <!-- Include Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
     <!-- jQuery UI CSS -->
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
@@ -47,7 +50,7 @@ $html = new HtmlHelper(new View());
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
     <!-- Custom CSS, JS -->
-    <?= $this->Html->css(['/vendor/fontawesome-free/css/all.min.css','style','default.css', 'cake']) ?>
+    <?= $this->Html->css(['/vendor/fontawesome-free/css/all.min.css','style','default.css', 'flash.message']) ?>
 
 
     <?= $this->Html->meta('icon', 'images/favicon.png', ['type' => 'icon']) ?>
