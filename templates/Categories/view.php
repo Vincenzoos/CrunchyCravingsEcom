70b96b46e7fb398667f8c1dc2f9117222df8349c
VTOD0001@student.monash.edu <vtod0001@student.monash.edu>
2025-04-25 22:46:54 +1000

    refactor (categories admin view): Seperated the related products table into a column on the right to reduce white space and minimise scrolling needed

--- templates/Categories/view.php
+++ templates/Categories/view.php
@@ -35,91 +35,91 @@ $html = new HtmlHelper(new \Cake\View\View());
         </section>
 
         <!-- Category Details Section -->
-        <section id="form-section" class="py-5">
+        <section id="form-section" class="">
             <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-6">
-                        <div id="form-content" class="text-center">
-                            <h3><?= h($category->name) ?></h3>
+                <div class="flex-container justify-content-center text-center">
+                    <!-- Left column -->
+                    <div id="form-content" class="col-md-6" style="height: 100%;">
+                        <h3 class="text-center">Name</h3>
+                        <table class="table table-bordered">
+                            <tr>
+                                <td><?= h($category->name) ?></td>
+                            </tr>
+                        </table>
+                        <div id="description-content" class="text-center mt-4">
+                            <h3><?= __('Description') ?></h3>
                             <table class="table table-bordered">
                                 <tr>
-                                    <th><?= __('Name') ?></th>
-                                    <td><?= h($category->name) ?></td>
+                                    <td><?= $this->Text->autoParagraph(h($category->description)); ?></td>
                                 </tr>
                             </table>
-                            <div id="description-content" class="text-center mt-4">
-                                <h3><?= __('Description') ?></h3>
-                                <table class="table table-bordered">
-                                    <tr>
-                                        <td><?= $this->Text->autoParagraph(h($category->description)); ?></td>
-                                    </tr>
-                                </table>
-                            </div>
                         </div>
+                    </div>
 
+                    <!-- Right column -->
+                    <div id="form-content" class="col-md-6" style="height: 100%;">
                         <!-- Related Products Section -->
                         <?php if (!empty($category->products)) : ?>
-                            <div id="form-content" class="mt-5">
-                                <h3 class="text-center">Related Products</h3>
-                                <div class="table-responsive">
-                                    <table class="table table-bordered table-hover">
-                                        <thead class="thead-dark">
+                            <h3 class="text-center">Related Products</h3>
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-hover">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th><?= __('Name') ?></th>
+                                            <th><?= __('Price') ?></th>
+                                            <th><?= __('Image') ?></th>
+                                            <th class="text-center"><?= __('Actions') ?></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach ($category->products as $product) : ?>
                                             <tr>
-                                                <th><?= __('Name') ?></th>
-                                                <th><?= __('Price') ?></th>
-                                                <th><?= __('Image') ?></th>
-                                                <th class="text-center"><?= __('Actions') ?></th>
+                                                <td><?= h($product->name) ?></td>
+                                                <td><?= $this->Number->currency($product->price, 'AUD', ['places' => 2]) ?></td>
+                                                <td><?= $this->Html->image($product->image_cache_busted_url, [
+                                                        'alt' => $product->name,
+                                                        'class' => 'img-fluid rounded',
+                                                        'style' => 'height: 100%; object-fit: cover; width: 100%;',
+                                                    ]) ?></td>
+                                                <td class="text-center">
+                                                    <?= $this->Html->link(__('View'), ['controller' => 'Products', 'action' => 'view', $product->id], ['class' => 'btn btn-info btn-sm']) ?>
+                                                    <?= $this->Html->link(__('Edit'), ['controller' => 'Products', 'action' => 'edit', $product->id], ['class' => 'btn btn-warning btn-sm']) ?>
+                                                    <?= $this->Form->postLink(
+                                                        __('Delete'),
+                                                        ['controller' => 'Products', 'action' => 'delete', $product->id],
+                                                        [
+                                                            'class' => 'btn btn-danger btn-sm',
+                                                            'confirm' => __('Are you sure you want to delete this product: {0}?', $product->name),
+                                                        ]
+                                                    ) ?>
+                                                </td>
                                             </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php foreach ($category->products as $product) : ?>
-                                                <tr>
-                                                    <td><?= h($product->name) ?></td>
-                                                    <td><?= $this->Number->currency($product->price, 'AUD', ['places' => 2]) ?></td>
-                                                    <td><?= $this->Html->image($product->image_cache_busted_url, [
-                                                            'alt' => $product->name,
-                                                            'class' => 'img-fluid rounded-top',
-                                                            'style' => 'height: 100%; object-fit: cover; width: 100%;',
-                                                        ]) ?></td>
-                                                    <td class="text-center">
-                                                        <?= $this->Html->link(__('View'), ['controller' => 'Products', 'action' => 'view', $product->id], ['class' => 'btn btn-info btn-sm']) ?>
-                                                        <?= $this->Html->link(__('Edit'), ['controller' => 'Products', 'action' => 'edit', $product->id], ['class' => 'btn btn-warning btn-sm']) ?>
-                                                        <?= $this->Form->postLink(
-                                                            __('Delete'),
-                                                            ['controller' => 'Products', 'action' => 'delete', $product->id],
-                                                            [
-                                                                'class' => 'btn btn-danger btn-sm',
-                                                                'confirm' => __('Are you sure you want to delete this product: {0}?', $product->name),
-                                                            ]
-                                                        ) ?>
-                                                    </td>
-                                                </tr>
-                                            <?php endforeach; ?>
-                                        </tbody>
-                                    </table>
-                                </div>
+                                        <?php endforeach; ?>
+                                    </tbody>
+                                </table>
                             </div>
                         <?php endif; ?>
 
-                        <!-- Actions -->
-                        <div class="text-center mt-4">
-                            <?= $this->Html->link('Edit Category', ['action' => 'edit', $category->id], ['class' => 'btn btn-warning']) ?>
-                            <?= $this->Form->postLink(
-                                'Delete Category',
-                                ['action' => 'delete', $category->id],
-                                [
-                                    'class' => 'btn btn-danger',
-                                    'confirm' => __('Are you sure you want to delete this category: {0}?', $category->name),
-                                ]
-                            ) ?>
-                        </div>
-                        <div class="text-center mt-4">
-                            <?= $this->Html->link('Back to Categories List', ['action' => 'index'], ['class' => 'btn btn-primary']) ?>
-                        </div>
                     </div>
                 </div>
             </div>
         </section>
+
+        <!-- Actions -->
+        <div class="text-center mt-4">
+            <?= $this->Html->link('Edit Category', ['action' => 'edit', $category->id], ['class' => 'btn btn-warning']) ?>
+            <?= $this->Form->postLink(
+                'Delete Category',
+                ['action' => 'delete', $category->id],
+                [
+                    'class' => 'btn btn-danger',
+                    'confirm' => __('Are you sure you want to delete this category: {0}?', $category->name),
+                ]
+            ) ?>
+        </div>
+        <div class="text-center mt-4">
+            <?= $this->Html->link('Back to Categories List', ['action' => 'index'], ['class' => 'btn btn-primary']) ?>
+        </div>
     </div>
 
     <!-- Bootstrap JS -->
